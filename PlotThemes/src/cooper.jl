cdb11823bd4aa0cd1a01ddd5d7e54d5c3bca182a
Ryan C Cooper <cooperrc@users.noreply.github.com>
2024-11-25 12:46:37 -0500

    Create cooper.jl
    adding custom `cooper` theme to Plots.jl

---
+++ PlotThemes/src/cooper.jl
@@ -0,0 +1,10 @@
+_themes[:cooper] = PlotTheme(linewidth = 4,
+                             markersize = 10,
+                             titlefontsize = 24,
+                             guidefontsize = 18,
+                             tickfontsize = 18,
+                             colorbar_tickfontsize = 18,
+                             legend_font_pointsize = 18,
+                             legend=:outertopright,
+                             grid = false
+                            )
